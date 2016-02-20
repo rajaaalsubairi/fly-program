34d87e9921a306b40f01b1e2e9da55fa163f2a80
Anna <holdikana@gmail.com>
2016-02-19 23:29:09 -0500

    feat(*): add toggle navigation

---
+++ js/push-menu.js
@@ -0,0 +1,17 @@
+$(function() {
+  $('body').removeClass('noscript');
+
+  $('.toggle-btn, .close-btn').click(function() {
+    toggleNav();
+  });
+});
+
+function toggleNav() {
+  if ($('.site-wrapper').attr('data-state') == 'slide-closed') {
+    // Display Nav when closed
+    $('.site-wrapper').attr('data-state', 'slide-open');
+  } else {
+    // Hide Nav when open
+    $('.site-wrapper').attr('data-state', 'slide-closed');
+  }
+}
